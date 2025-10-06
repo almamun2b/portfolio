713935eac50a4c511e32c0bf4d7574f23c03248b
almamun2b <almamun2b@gmail.com>
2025-10-06 23:42:16 +0600

    test

---
+++ src/app/middleware.ts
@@ -0,0 +1,28 @@
+export { default } from "next-auth/middleware";
+
+import { getToken } from "next-auth/jwt";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+// export const config = { matcher: ["/dashboard"] };
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  console.log("Token:", token);
+
+  if (!token) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
